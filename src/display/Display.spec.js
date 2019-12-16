3c090e2fd485bcb88e4a155fd32654f6f4cefa15
nathan-saygers <nathan.saygers@gmail.com>
2019-12-16 14:15:39 -0800

    got the first two tests running

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from '../dashboard/Dashboard';
+
+test('On render, gate is unlocked and open', () => {
+  const {findByText} = render(<Dashboard />); 
+  findByText(/unlocked/i);
+  findByText(/open/i);
+  // let lockerBtn = 
+  // console.log('locker button', lockerBtn);
+  // expect(lockerBtn).toHaveClass('green-led');
+})
+
+it('cannot be closed or opened if it is locked', () => {
+  const {getByText} = render(<Dashboard />);
+  let openCloseBtn = getByText(/open\ gate|close\ gate/i);
+  let lockUnlockBtn = getByText(/unlock\ gate|lock\ gate/i);
+  fireEvent.click(openCloseBtn);
+  fireEvent.click(lockUnlockBtn);
+  fireEvent.click(openCloseBtn);
+  console.log('Has Open Close button resolved', openCloseBtn)
+  expect(openCloseBtn.textContent).toBe('Open Gate');
+});
