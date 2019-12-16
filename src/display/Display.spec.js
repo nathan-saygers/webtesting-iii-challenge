219afbd79445e9ae484e238298cbf8da9b8eb351
nathan-saygers <nathan.saygers@gmail.com>
2019-12-16 15:03:09 -0800

    completed controls testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import Dashboard from '../dashboard/Dashboard';
+import Display from './Display';
 
-test('On render, gate is unlocked and open', () => {
-  const {findByText} = render(<Dashboard />); 
-  findByText(/unlocked/i);
-  findByText(/open/i);
-  // let lockerBtn = 
-  // console.log('locker button', lockerBtn);
-  // expect(lockerBtn).toHaveClass('green-led');
+test('displays Closed if the closed prop is true and Open if otherwise', () => {
+  const {getByText} = render(<Display closed={true} />); 
+  let openCloseDisp = getByText(/open|closed/i);
+  expect(openCloseDisp.textContent).toBe('Closed');
 })
 
-it('cannot be closed or opened if it is locked', () => {
-  const {getByText} = render(<Dashboard />);
-  let openCloseBtn = getByText(/open\ gate|close\ gate/i);
-  let lockUnlockBtn = getByText(/unlock\ gate|lock\ gate/i);
-  fireEvent.click(openCloseBtn);
-  fireEvent.click(lockUnlockBtn);
-  fireEvent.click(openCloseBtn);
-  console.log('Has Open Close button resolved', openCloseBtn)
-  expect(openCloseBtn.textContent).toBe('Open Gate');
-});
+it('displays Locked if the locked prop is true and Unlocked if otherwise', () => {
+  const {getByText} = render(<Display locked={true} />); 
+  let lockedUnlockedDisp = getByText(/unlocked|locked/i);
+  expect(lockedUnlockedDisp.textContent).toBe('Locked');
+})
+
+it('when locked or closed use the red-led class', () => {
+  const {getByText} = render(<Display locked={true} closed={true}/>); 
+  let lockedUnlockedDisp = getByText(/unlocked|locked/i);
+  let openCloseDisp = getByText(/open|closed/i);
+  expect(lockedUnlockedDisp.className).toBe('led red-led');
+  expect(openCloseDisp.className).toContain('red-led');
+})
+
+it('when unlocked or open use the green-led class', () => {
+  const {getByText} = render(<Display locked={false} closed={false}/>); 
+  let lockedUnlockedDisp = getByText(/unlocked|locked/i);
+  let openCloseDisp = getByText(/open|closed/i);
+  expect(lockedUnlockedDisp.className).toContain('green-led');
+  expect(openCloseDisp.className).toContain('green-led');
+})
